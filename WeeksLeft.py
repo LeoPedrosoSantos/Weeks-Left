15acf74150787277f76c7d0e67a7cc1626c48434
Leonardo Pedroso dos Santos <leonardopedroso27@yahoo.com.br>
2024-01-12 21:48:53 -0300

    Weeks Left

---
+++ WeeksLeft.py
@@ -0,0 +1,15 @@
+print("***Weeks left to 90 years***\n")
+Age = int(input("How old are you?\n"))
+
+OneYear = 52.1786
+
+Calculate = Age * OneYear
+
+Years = 4696.07
+
+Result = Years - Calculate
+
+print("\nYou have " + str(round(Result ,2)) + " weeks left to 90 years")
+
+print(input("\nClick ENTER to close the window"))
+
